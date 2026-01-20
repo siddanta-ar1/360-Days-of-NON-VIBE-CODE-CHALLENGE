ff6cadd716e7471274132f5cd361bde154daded0
Siddanta Sodari <siddantasodari123@gmail.com>
2026-01-20 22:35:22 +0545

    Express implementation

---
+++ Jan20/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Express Server!");
+});
+
+app.post("/login", (req, res) => {
+  const { name, password } = req.body;
+  console.log("User Data:", req.body);
+
+  res.json({
+    message: `Hello ${name}, you are logged in!`,
+    status: "Success",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
