5b1726c45b773b3c34183c3e8cfb8de3be42ce5e
Siddanta Sodari <siddantasodari123@gmail.com>
2026-01-21 22:59:51 +0545

    Concept of Middleware

---
+++ Jan21/middleware.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+const loggerMiddleware = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().toISOString();
+  console.log(`[${time}] ${method} ${url}`);
+  next();
+};
+app.use(loggerMiddleware);
+
+const apiKeyMiddleware = (req, res, next) => {
+  const apiKey = req.query.key;
+
+  if (apiKey === "123") {
+    console.log("Access Granted.");
+    next();
+  } else {
+    console.log("Access Denied.");
+    res.status(403).json({ error: "FOrbidden: Wrong Key" });
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Public Home Page.");
+});
+
+app.get("/dashboard", apiKeyMiddleware, (req, res) => {
+  res.send("Welcome to the Secret Dashboard! ");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
