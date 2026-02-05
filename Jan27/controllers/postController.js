7c15a3785b4de206689be871dd7575527eb2efd0
Siddanta Sodari <siddantasodari123@gmail.com>
2026-02-06 01:11:59 +0545

    Concept of Pagination

--- Jan27/controllers/postController.js
+++ Jan27/controllers/postController.js
@@ -26,14 +26,25 @@ const createPost = async (req, res) => {
 
 const getAllPosts = async (req, res) => {
   try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    const offset = (page - 1) * limit;
     const query = `
       SELECT posts.id, posts.title, posts.content, users.username
       FROM posts
       JOIN users ON posts.user_id = users.id
-      ORDER BY posts.created_at DESC;
+      ORDER BY posts.created_at DESC
+      LIMIT $1 OFFSET $2;
       `;
-    const result = await db.query(query);
-    res.json({ success: true, posts: result.rows });
+    const result = await db.query(query, [limit, offset]);
+    res.json({
+      success: true,
+      page: page,
+      limit: limit,
+      count: result.rows.length,
+      posts: result.rows,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server Error" });
