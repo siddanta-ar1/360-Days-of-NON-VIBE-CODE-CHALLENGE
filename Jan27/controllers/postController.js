3f99a8c57fb5a440f685e0288eab24d6c1f95dbd
Siddanta Sodari <siddantasodari123@gmail.com>
2026-02-24 22:30:11 +0545

    N+1 Deep Dive

--- Jan27/controllers/postController.js
+++ Jan27/controllers/postController.js
@@ -60,6 +60,55 @@ const getAllPosts = asyncHandler(async (req, res) => {
   });
 });
 
+const getPostById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const query = `
+    SELECT p.id, p.title, p.content, p.created_at,
+    --1. Nest the Author as a JSON json_build_object
+    json_build_object(
+    'id', u.id,
+    'username', u.username,
+    'profile_pic', u.profile_pic
+    ) AS author,
+
+    -- 2. Count the likes
+    (SELECT COUNT(*) FROM likes l WHERE l.post_id = p.id) AS like_count,
+
+    -- 3. Nest all comments as a JSON array (Handling 0 comments with COALESCE)
+    COALESCE(
+    (
+    SELECT json_agg(
+    json_build_object(
+    'id', c.id,
+    'content', c.content,
+    'username', cu.username,
+    'created_at', c.created_at
+    ) ORDER BY c.created_at ASC
+    )
+    FROM comments c
+    JOIN users cu ON c.user_id = cu.id
+    WHERE c.post_id = p.id
+    ), '[]'
+    ) AS comments
+
+    FROM posts p
+    JOIN users u ON p.user_id = u.id --INNER JOIN is fine here (every post HAS to have an author)
+    WHERE p.id = $1;
+    `;
+  const result = await db.query(query, [id]);
+
+  if (result.rows.length === 0) {
+    res.status(404);
+    throw new Error("Post not found");
+  }
+
+  res.json({
+    success: true,
+    post: result.rows[0],
+  });
+});
+
 const updatePost = asyncHandler(async (req, res) => {
   const { id } = req.params;
   const { title, content } = req.body;
