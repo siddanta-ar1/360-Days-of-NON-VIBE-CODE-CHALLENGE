8dfde0da16f115a2d3b9a761cc3d11b2b7e8a125
Siddanta Sodari <siddantasodari123@gmail.com>
2026-03-19 23:41:18 +0545

    Stateless APIs AND Distributed In-Memory Cashing.

--- Jan27/middleware/customLimiter.js
+++ Jan27/middleware/customLimiter.js
@@ -1,5 +1,43 @@
 const rateLimits = new Map();
+const Redis = require("ioredis");
 
+const redis = new Redis({
+  host: "127.0.0.1",
+  port: 6379,
+});
+
+const redisTokenBucket = async (req, res, next) => {
+  const ip = req.ip;
+  const CAPACITY = 5;
+  const REFILL_RATE_MS = 1000;
+  const now = Date.now();
+
+  let rawBucket = await redis.get(ip);
+  let bucket;
+
+  if (!rawBucket) {
+    bucket = { tokens: CAPACITY, lastRefill: now };
+  } else {
+    bucket = JSON.parse(rawBucket);
+  }
+
+  const timePassed = now - bucket.lastRefill;
+  const tokensToAdd = Math.floor(timePassed / REFILL_RATE_MS);
+
+  if (tokensToAdd > 0) {
+    bucket.tokens = Math.min(CAPACITY, bucket.tokens + tokensToAdd);
+    bucket.lastRefill = now;
+  }
+
+  if (bucket.tokens > 0) {
+    bucket.tokens -= 1;
+    await redis.set(ip, JSON.stringify(bucket), "PX", 10000);
+
+    next();
+  } else {
+    res.status(429).json({ error: "Too Many Requests. Blocked by Redis." });
+  }
+};
 const tokenBucketLimiter = (req, res, next) => {
   const ip = req.ip;
   const now = Date.now();
@@ -29,4 +67,4 @@ const tokenBucketLimiter = (req, res, next) => {
   }
 };
 
-module.exports = { tokenBucketLimiter };
+module.exports = { tokenBucketLimiter, redisTokenBucket };
